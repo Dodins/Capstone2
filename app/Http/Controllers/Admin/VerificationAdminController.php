7abe01bdbe2c567d3df380deec7f2b63d9d3fc81
unknown <diag.devillena.up@phinmaed.com>
2025-03-14 20:48:26 +0800

    Verification Request

---
+++ app/Http/Controllers/Admin/VerificationAdminController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Resident;
+use Illuminate\Http\Request;
+
+class VerificationAdminController extends Controller
+{
+    public function unverifiedResidents()
+    {
+        $residents = Resident::where('is_verified', false)->where('application_status', 'pending')->get();
+
+        if($residents->isEmpty()){
+            return response()->json(
+                ['message' => 'No Pending Verification request',
+            ]);
+        }
+        return response()->json($residents);
+    }
+
+    public function accept($id)
+    {
+        $resident = Resident::findOrFail($id);
+
+        $resident->update([
+            'is_verified' => true,
+            'application_status' => 'approved',
+        ]);
+
+        return response()->json([
+            'message' => 'The person is verified'
+        ]);
+    }
+
+    public function reject($id)
+    {
+        $resident = Resident::findOrFail($id);
+
+        $resident->update([
+            'is_verified' => false,
+            'application_status' => 'rejected',
+        ]);
+
+        return response()->json([
+            'message' => 'The person is rejected'
+        ]);
+    }
+}
+
