8c23543b4b4e4a91ff6c8f48b08583722992beb3
Khun01 <johnbrandonlambino123@gmail.com>
2025-03-26 00:04:12 +0800

    Added card for incoming reports

---
+++ resources/js/Components/Reports/ReportCard.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { ref } from "vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import SecondaryButton from "@/Components/SecondaryButton.vue";
+
+const props = defineProps({
+    title: String,
+    incomingConcerns: Array,
+});
+
+const priority = ref("Choose");
+const options = ["High", "Medium", "Low"];
+</script>
+
+<template>
+    <div class="border-3 dark:border-[#3C4053] border-[#DDE3E7] p-4 rounded-lg">
+        <div class="flex w-full h-full gap-4 items-center">
+            <div
+                class="rounded-xl dark:bg-[#3C4053] bg-[#DDE3E7] h-24 w-24"
+            ></div>
+            <div class="flex flex-col justify-between flex-1">
+                <h1
+                    class="w-1/4 dark:text-[#EEEEEE] text-[#222831] font-bold text-[18px]"
+                >
+                    {{ props.title }}
+                </h1>
+                <h1
+                    class="w-2/4 dark:text-[#B0B0B0] text-[#4B5660] text-[16px]"
+                >
+                    hahahahhahahahah
+                    hehehehehehehehehehehehehehehehehehehehehehe
+                </h1>
+                <h1
+                    class="w-1/4 dark:text-[#B0B0B0] text-[#4B5660] text-[16px]"
+                >
+                    January 1, 2025
+                </h1>
+            </div>
+            <div class="flex flex-col gap-2">
+                <PrimaryButton> Accept </PrimaryButton>
+                <SecondaryButton> Reject </SecondaryButton>
+            </div>
+        </div>
+    </div>
+</template>
